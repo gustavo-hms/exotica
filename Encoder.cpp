2f8b55861f6d1c81bcf3748cb86c78d47e5c20f5
Gustavo Henrique Montesião de Sousa <gustavo@registro.br>
2014-11-10 10:28:09 -0200

    Finish the recursion in encode method

--- Encoder.cpp
+++ Encoder.cpp
@@ -6,10 +6,9 @@
 #include <QXmlStreamWriter>
 #include "Encoder.h"
 
-Encoder::Encoder(QIODevice* out) :
-	_out(out) {
-		_stream = new QXmlStreamWriter(out);
-	}
+Encoder::Encoder(QIODevice* out) : _out(out) {
+	_stream = new QXmlStreamWriter(out);
+}
 
 Encoder::Encoder(QIODevice* out, int indentationLevel) : _out(out) {
 	_stream = new QXmlStreamWriter(out);
@@ -34,13 +33,13 @@ bool Encoder::encode(QObject* object) {
 
 	int versionIndex = meta->indexOfClassInfo("xmlVersion");
 	QString version =
-		versionIndex != -1 ? meta->classInfo(versionIndex).value() : "1.0";
+	    versionIndex != -1 ? meta->classInfo(versionIndex).value() : "1.0";
 
 	int standaloneIndex = meta->indexOfClassInfo("xmlStandalone");
 	bool standalone = false;
 
 	if (standaloneIndex != -1
-		&& QString(meta->classInfo(standaloneIndex).value()) == "true") {
+	    && QString(meta->classInfo(standaloneIndex).value()) == "true") {
 		standalone = true;
 	}
 
@@ -54,7 +53,7 @@ void Encoder::encode(QObject* object, const QString& tagName) {
 	auto meta = object->metaObject();
 
 	int marshalMethod =
-		meta->indexOfMethod(QMetaObject::normalizedSignature("marshalXML(QIODevice*)"));
+	    meta->indexOfMethod(QMetaObject::normalizedSignature("marshalXML(QIODevice*)"));
 
 	if (marshalMethod != -1) {
 		bool returnValue;
@@ -85,7 +84,7 @@ void Encoder::encode(QObject* object, const QString& tagName) {
 		auto namespaceAndPrefix = xmlNamespace.split(" ", QString::SkipEmptyParts);
 
 		if (namespaceAndPrefix.size() > 1
-			&& namespaceAndPrefix[0] != _currentNamespace) {
+		    && namespaceAndPrefix[0] != _currentNamespace) {
 			_stream->writeNamespace(namespaceAndPrefix[0], namespaceAndPrefix[1]);
 			_currentNamespace = namespaceAndPrefix[0];
 		}
@@ -100,8 +99,22 @@ void Encoder::encode(QObject* object, const QString& tagName) {
 	_stream->writeEndElement();
 }
 
-void Encoder::encode(const QVariant& obj, const QString& tagName)
-{
+void Encoder::encode(const QVariant& obj, const QString& tagName) {
+	if (obj.canConvert<QObject*>()) {
+		encode(obj.value<QObject*>(), tagName);
+		return;
+	}
+
+	if (obj.canConvert<QVariantList>()) {
+		for (auto element : obj.value<QSequentialIterable>()) {
+			encode(element, tagName);
+		}
+
+		return;
+	}
+
+	QString xmlName;
+
 	switch (obj.userType()) {
 	case QMetaType::QString:
 	case QMetaType::Bool:
@@ -112,12 +125,9 @@ void Encoder::encode(const QVariant& obj, const QString& tagName)
 	case QMetaType::Double:
 	case QMetaType::Int:
 	case QMetaType::LongLong:
-	case QMetaType::QStringList:
 	case QMetaType::QTime:
 	case QMetaType::UInt:
 	case QMetaType::ULongLong:
-		QString xmlName;
-
 		if (tagName.size()) {
 			xmlName = tagName;
 
