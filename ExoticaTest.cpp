b160626216014e291a34a637c177cf1fcbdcaf60
Gustavo Henrique Montesião de Sousa <gustavo@cecm.usp.br>
2014-11-18 10:29:58 -0200

    Some tests

---
+++ ExoticaTest.cpp
@@ -0,0 +1,32 @@
+#include "ExoticaTest.h"
+#include "exotica.h"
+
+ObjectA1* objectA1 = new ObjectA1 {17, 17.17, "Claro enigma"};
+ObjectA2* objectA2 = new ObjectA2 {objectA1, {1, 2, 3}};
+ObjectA3* objectA3 = new ObjectA3 {{1.2, 1.3}, objectA2};
+QString objectA3XML =
+    R"(<?xml version="1.0" encoding="UTF-8" standalone="no"?><ObjectA3><a31>1.2</a31><a31>1.3</a31><a32><a21><a11>17</a11><a12>17.17</a12><a13>Claro enigma</a13></a21><a22>1</a22><a22>2</a22><a22>3</a22></a32></ObjectA3>)" "\n";
+
+ObjectA4* objectA4 = new ObjectA4 {{1.2, 1.3}, objectA2};
+QString objectA4XML =
+    R"(<?xml version="1.0" encoding="UTF-8" standalone="no"?><object><a31>1.2</a31><a31>1.3</a31><a32><a21><a11>17</a11><a12>17.17</a12><a13>Claro enigma</a13></a21><a22>1</a22><a22>2</a22><a22>3</a22></a32></object>)" "\n";
+
+void ExoticaTest::marshalObject_data() {
+    QTest::addColumn<QObject*>("object");
+    QTest::addColumn<QString>("xml");
+    QTest::newRow("Simple structure of objects") << (QObject*) objectA3 << objectA3XML;
+       QTest::newRow("Object with a tag name") << (QObject*) objectA4 << objectA4XML;
+}
+
+void ExoticaTest::marshalObject() {
+    QFETCH(QObject*, object);
+    QFETCH(QString, xml);
+    QBuffer buffer;
+    buffer.open(QBuffer::WriteOnly);
+    marshal(object, &buffer);
+    QString result = buffer.data();
+    QCOMPARE(result, xml);
+    buffer.close();
+}
+
+QTEST_APPLESS_MAIN(ExoticaTest)
--- ExoticaTest.h
+++ ExoticaTest.h
@@ -1,12 +1,82 @@
 #ifndef EXOTICA_TEST_H
 #define EXOTICA_TEST_H
 
+#include <QObject>
+#include <QList>
+#include <QVector>
+#include <QString>
 #include <QtTest/QtTest>
 
 class ExoticaTest : public QObject {
-	Q_OBJECT
+    Q_OBJECT
 private slots:
-	void marshal();
+    void marshalObject();
+    void marshalObject_data();
+};
+
+// Simple structure of objects
+
+class ObjectA1 : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(int a11 MEMBER member1)
+    Q_PROPERTY(double a12 MEMBER member2)
+    Q_PROPERTY(QString a13 MEMBER member3)
+
+public:
+    ObjectA1(QObject* parent = nullptr) : QObject(parent) {}
+    ObjectA1(int m1, double m2, const QString& m3) :
+        QObject(nullptr),
+        member1(m1),
+        member2(m2),
+        member3(m3) {}
+
+    int member1;
+    double member2;
+    QString member3;
+};
+
+class ObjectA2 : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(ObjectA1* a21 MEMBER member1)
+    Q_PROPERTY(QVector<int> a22 MEMBER member2)
+
+public:
+    ObjectA2(QObject* parent = nullptr) : QObject(parent) {}
+    ObjectA2(ObjectA1* m1, const QVector<int>& m2) :
+        QObject(nullptr),
+        member1(m1),
+        member2(m2) {}
+
+    ObjectA1* member1;
+    QVector<int> member2;
+};
+
+class ObjectA3 : public QObject {
+    Q_OBJECT
+    Q_PROPERTY(QList<double> a31 MEMBER member1)
+    Q_PROPERTY(ObjectA2* a32 MEMBER member2)
+
+public:
+    ObjectA3(QObject* parent = nullptr) : QObject(parent) {}
+    ObjectA3(const QList<double>& m1, ObjectA2* m2) :
+        QObject(nullptr),
+        member1(m1),
+        member2(m2) {}
+
+    QList<double> member1;
+    ObjectA2* member2;
+};
+
+// Object with a tag name
+
+class ObjectA4 : public ObjectA3 {
+    Q_OBJECT
+    Q_CLASSINFO("xmlName", "object")
+    Q_PROPERTY(QList<double> a31 MEMBER member1)
+    Q_PROPERTY(ObjectA2* a32 MEMBER member2)
+
+public:
+    using ObjectA3::ObjectA3;
 };
 
 #endif // EXOTICA_TEST_H
